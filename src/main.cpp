6d36d8184777cd30828aceb9fd2235465d09e2df
jesus <theaminishere@gmail.com>
2026-09-12 17:24:00 +0530

    huge refactor and tryin to learn barnes-hut algo

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include "raylib.h"
-#include <cstdlib>
 #include <glm/detail/qualifier.hpp>
 #include <glm/ext/vector_float3.hpp>
 #include <glm/geometric.hpp>
 #include <glm/glm.hpp>
-#include <iostream>
-#include <vector>
+
+using namespace glm;
 
 // constexpr double G = 6.67430e-11;
-constexpr double G = 1;
-using namespace glm;
+constexpr float G = 1.0f;
 
 struct body {
   vec3 position;
@@ -20,84 +18,120 @@ struct body {
 
 vec3 force_on(const body &A, const body &B) {
   vec3 displacement = B.position - A.position;
-  float r = glm::length(displacement);
+
+  float r = length(displacement);
+
+  if (r < 0.0001f)
+    return vec3(0.0f);
+
   float magnitude = (G * A.mass * B.mass) / (r * r);
-  vec3 direction = glm::normalize(displacement);
+
+  vec3 direction = displacement / r;
+
   return magnitude * direction;
 }
 
-void render(float Ax, float Ay, float Bx, float By);
-
 int main() {
+  InitWindow(800, 600, "2Body");
+  SetTargetFPS(60);
 
-  InitWindow(800, 600, "2body");
-  SetTargetFPS(120);
-  // body A, B;
-  // A.position = vec3(0, 0, 0);
-  // A.velocity = vec3(0, 0, 0);
-  // A.acceleration = vec3(0, 0, 0);
-  // A.mass = 1000;
-  //
-  // B.position = vec3(100, 0, 0);
-  // B.velocity = vec3(0, 0, 0);
-  // B.acceleration = vec3(0, 0, 0);
-  // B.mass = 1000;
-  //
+  body A{};
+  body B{};
+
+  A.position = vec3(200.0f, 300.0f, 0.0f);
+  A.velocity = vec3(0.0f);
+  A.acceleration = vec3(0.0f);
+  A.mass = 100000.0f;
+
+  B.position = vec3(600.0f, 300.0f, 0.0f);
+  B.velocity = vec3(0.0f, 10.0f, 0.0f);
+  B.acceleration = vec3(0.0f);
+  B.mass = 1000.0f;
+
+  float dt = 0.05f;
+
+  /*
   int n = 1000;
 
   std::vector<body> bodies;
 
   for (int i = 0; i < n; i++) {
-    body a;
-    a.position = vec3(rand() % 800, rand() % 600, rand() % 300);
-    a.mass = 1000;
-    bodies.push_back(a);
+
+      body a{};
+
+      a.position = vec3(
+          rand() % 800,
+          rand() % 600,
+          rand() % 300
+      );
+
+      a.velocity = vec3(0.0f);
+      a.acceleration = vec3(0.0f);
+
+      a.mass = 1000.0f;
+
+      bodies.push_back(a);
   }
+  */
 
   while (!WindowShouldClose()) {
 
-    // float dt = 1;
-    // A.acceleration = force_on(A, B) / A.mass;
-    // A.velocity += A.acceleration * dt;
-    // A.position += A.velocity * dt;
-    //
-    // B.acceleration = force_on(B, A) / B.mass;
-    // B.velocity += B.acceleration * dt;
-    // B.position += B.velocity * dt;
-    //
-    // render(A.position.x, A.position.y, B.position.x, B.position.y);
+    vec3 force_A = force_on(A, B);
 
-    float dt = 0.05f;
+    vec3 force_B = force_on(B, A);
+
+    A.acceleration = force_A / A.mass;
+
+    B.acceleration = force_B / B.mass;
+
+    A.velocity += A.acceleration * dt;
+
+    B.velocity += B.acceleration * dt;
+
+    A.position += A.velocity * dt;
+
+    B.position += B.velocity * dt;
+
+    /*
+    for (int i = 0; i < n; i++) {
+
+        bodies[i].acceleration = vec3(0.0f);
+
+        for (int j = 0; j < n; j++) {
+
+            if (i == j)
+                continue;
+
+            bodies[i].acceleration +=
+                force_on(bodies[i], bodies[j])
+                / bodies[i].mass;
+        }
+    }
+
 
     for (int i = 0; i < n; i++) {
 
-      bodies[i].acceleration = vec3(0);
+        bodies[i].velocity +=
+            bodies[i].acceleration * dt;
 
-      for (int j = 0; j < n; j++) {
-
-        if (i == j)
-          continue;
-
-        bodies[i].acceleration +=
-            force_on(bodies[i], bodies[j]) / bodies[i].mass;
-      }
+        bodies[i].position +=
+            bodies[i].velocity * dt;
     }
-
-    for (int i = 0; i < n; i++) {
-      bodies[i].velocity += bodies[i].acceleration * dt;
-      bodies[i].position += bodies[i].velocity * dt;
-    }
+    */
 
     BeginDrawing();
 
     ClearBackground(BLACK);
 
-    for (int i = 0; i < n; i++) {
-      DrawCircle(bodies[i].position.x, bodies[i].position.y, 3, WHITE);
-    }
+    // Draw A
+    DrawCircle((int)A.position.x, (int)A.position.y, 8, WHITE);
+
+    DrawCircle((int)B.position.x, (int)B.position.y, 8, WHITE);
 
     EndDrawing();
   }
+
+  CloseWindow();
 
   return 0;
 }
