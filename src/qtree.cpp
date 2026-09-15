36ac14f3fc0d74c883f3cbc16314a0d1526759eb
jesus <theaminishere@gmail.com>
2026-09-15 23:34:43 +0530

    :D

--- src/qtree.cpp
+++ src/qtree.cpp
@@ -1,11 +1,10 @@
-#include "raylib.h"
-#include <cstdlib>
+
 #include <vector>
 
-// points
 class point {
 private:
-  double x = 0, y = 0;
+  double x = 0;
+  double y = 0;
 
 public:
   point(double _x, double _y) : x(_x), y(_y) {}
@@ -14,25 +13,28 @@ public:
   double getY() const { return y; }
 };
 
-// rectangle
 class rect {
 private:
-  double x_start, y_start;
-  double width, height;
+  double x_start;
+  double y_start;
+  double width;
+  double height;
 
 public:
   rect(double _x, double _y, double _width, double _height)
       : x_start(_x), y_start(_y), width(_width), height(_height) {}
+
   friend class node;
 };
 
-// node
 class node {
-
 private:
   const int N_MAX = 1;
+
   std::vector<point> points;
+
   rect boundary;
+
   node *nw;
   node *ne;
   node *sw;
@@ -45,11 +47,12 @@ public:
     delete sw;
     delete se;
   }
+
   node(rect _boundary)
       : boundary(_boundary), nw(nullptr), ne(nullptr), sw(nullptr),
         se(nullptr) {}
 
-  bool contains(const point &p) {
+  bool contains(const point &p) const {
     return p.getX() >= boundary.x_start &&
            p.getX() < boundary.x_start + boundary.width &&
            p.getY() >= boundary.y_start &&
@@ -57,11 +60,12 @@ public:
   }
 
   void subdivide() {
-    //_boundary becomes _boundary/2
     nw = new node(rect(boundary.x_start, boundary.y_start, boundary.width / 2,
                        boundary.height / 2));
+
     ne = new node(rect(boundary.x_start + boundary.width / 2, boundary.y_start,
                        boundary.width / 2, boundary.height / 2));
+
     sw = new node(rect(boundary.x_start, boundary.y_start + boundary.height / 2,
                        boundary.width / 2, boundary.height / 2));
 
@@ -73,6 +77,7 @@ public:
   node *get_quadrant(const point &p) {
 
     double x_mid = boundary.x_start + boundary.width / 2;
+
     double y_mid = boundary.y_start + boundary.height / 2;
 
     if (p.getX() < x_mid && p.getY() < y_mid)
@@ -93,82 +98,36 @@ public:
       return;
     }
 
-    // node has room
+    // Node has room.
     if (points.size() < N_MAX) {
       points.push_back(p);
       return;
     }
 
-    // node is full
+    // Node is full.
     if (nw == nullptr) {
       subdivide();
     }
 
-    // move existing point down
+    // Move existing point down.
     point old_point = points[0];
     points.clear();
 
     get_quadrant(old_point)->insert(old_point);
 
-    // insert new point
+    // Insert new point.
     get_quadrant(p)->insert(p);
-  }
-  void draw() const {
-
-    DrawRectangleLines(boundary.x_start, boundary.y_start, boundary.width,
-                       boundary.height, WHITE);
-
-    for (const point &p : points) {
-      DrawCircle(p.getX(), p.getY(), 4, GREEN);
-    }
-
-    if (nw != nullptr) {
-      nw->draw();
-      ne->draw();
-      sw->draw();
-      se->draw();
-    }
   }
 };
 
-// tree
 class qtree {
 private:
   node *root;
 
 public:
   qtree(rect box) : root(new node(box)) {}
+
   ~qtree() { delete root; }
+
   void qinsert(const point &p) { root->insert(p); }
-  void draw() const { root->draw(); }
 };
-
-int main() {
-
-  InitWindow(1800, 1000, "Quadtree");
-
-  qtree tree(rect(0, 0, 1800, 1000));
-
-  // tree.qinsert(point(100, 100));
-  // tree.qinsert(point(500, 200));
-  // tree.qinsert(point(300, 600));
-  // tree.qinsert(point(700, 700));
-
-  for (int i = 0; i < 1999; i++) {
-
-    tree.qinsert(point(rand() % 1800, rand() % 1000));
-  }
-
-  while (!WindowShouldClose()) {
-
-    BeginDrawing();
-
-    ClearBackground(BLACK);
-
-    tree.draw();
-
-    EndDrawing();
-  }
-
-  CloseWindow();
-}
